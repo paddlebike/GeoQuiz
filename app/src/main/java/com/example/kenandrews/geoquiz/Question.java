be460841ad18748bab9f8869ef54d7e5fa176196
Ken Andrews <paddlebike@yahoo.com>
2017-07-19 21:45:38 -0400

    Initial Check-in
    Complete to Figure 2.9

---
+++ app/src/main/java/com/example/kenandrews/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.example.kenandrews.geoquiz;
+
+/**
+ * Created by kenandrews on 7/13/17.
+ * From Chapter 2 of Android Programming : The Big Nerd Ranch
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
